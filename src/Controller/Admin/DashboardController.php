d72c6a4d3e9089c93a762b2412a49dbbc37f9872
maxshdev <max.shtefec@gmail.com>
2022-11-22 11:55:30 -0300

    first commit

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Player;
+
+use App\Controller\Admin\PlayerCrudController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+
+        return $this->redirect($routeBuilder->setController(PlayerCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('TennisTournamentTest');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Home', 'fa fa-home');
+        
+        yield MenuItem::subMenu('Tournament', 'fa fa-list')->setSubItems([
+            MenuItem::linkToCrud('Players', 'fas fa-cash-register', Player::class),
+        ]);
+    }
+}
---
+++ src/Entity/Player.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlayerRepository::class)]
+class Player
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $hability_lv = null;
+
+    #[ORM\Column(length: 1)]
+    private ?string $sex = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $strength = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $displacement_velocity = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $reaction_time = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getHabilityLv(): ?int
+    {
+        return $this->hability_lv;
+    }
+
+    public function setHabilityLv(int $hability_lv): self
+    {
+        $this->hability_lv = $hability_lv;
+
+        return $this;
+    }
+
+    public function getSex(): ?string
+    {
+        return $this->sex;
+    }
+
+    public function setSex(string $sex): self
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(?int $strength): self
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    public function getDisplacementVelocity(): ?int
+    {
+        return $this->displacement_velocity;
+    }
+
+    public function setDisplacementVelocity(?int $displacement_velocity): self
+    {
+        $this->displacement_velocity = $displacement_velocity;
+
+        return $this;
+    }
+
+    public function getReactionTime(): ?int
+    {
+        return $this->reaction_time;
+    }
+
+    public function setReactionTime(?int $reaction_time): self
+    {
+        $this->reaction_time = $reaction_time;
+
+        return $this;
+    }
+}
