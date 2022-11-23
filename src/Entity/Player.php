87f80a217461c7856d0abd0e5ec8fbdcaea92d11
maxshdev <max.shtefec@gmail.com>
2022-11-22 22:14:52 -0300

    update config and api platform

--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -4,7 +4,9 @@ namespace App\Entity;
 
 use App\Repository\PlayerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: PlayerRepository::class)]
 class Player
 {
